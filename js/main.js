84aac60a91e6508470623ef8249dd42784da5967
forteller11 <charly.yan.miller@gmail.com>
2019-09-09 22:07:15 -0400

    got webgl context

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 'use strict';
 
+
 //var window = getElementsByClassName('Window');
 window.onload = main;
+var canvas;
+var gl;
+
 
 console.log("ah");
 
 function main(){
+  canvas = document.getElementById("canvasId");
+  gl = canvas.getContext("webgl");
+
+console.log(window);
+  if (gl == null)
+  {
+    console.error("Couldn't get webGL context!");
+    return;
+  }
+
+
+
+
+
+  console.log(window);
   console.log(glMatrix);
   console.log(glMatrix.glMatrix.toRadian(90));
+  console.log(canvas);
 }
