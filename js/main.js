5f51c866bc3cf5250527a9c141b4e8e6ddca41b2
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-09-11 09:26:10 -0400

    cart 351 got create program working
    just using strings instead of scripts temporarily

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 
-//var window = getElementsByClassName('Window');
+//let window = getElementsByClassName('Window');
 window.onload = main;
-var canvas;
-var gl;
+let canvas;
+let gl;
 
 
 console.log("ah");
@@ -27,17 +27,18 @@ console.log(gl);
   gl.clearColor(1, 0, 1, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var vertexShaderSourceCode = document.getElementById("2d-vertex-shader").text;
-  var fragmentShaderSourceCode = document.getElementById("2d-fragment-shader").text;
+  let vertexShaderSourceCode = document.getElementById("2d-vertex-shader").text;
+  let fragmentShaderSourceCode = document.getElementById("2d-fragment-shader").text;
+  console.log(typeof(vertexShaderSourceCode));
   console.log(vertexShaderSourceCode);
   console.log(window);
 
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSourceCode);
-  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSourceCode);
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertString);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragString);
 
-  var program = createProgram(gl, vertexShader, fragmentShader);
+  let program = createProgram(gl, vertexShader, fragmentShader);
 
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
   console.log(window);
   console.log(glMatrix);
   console.log(glMatrix.glMatrix.toRadian(90));
@@ -46,10 +47,10 @@ console.log(gl);
 
 
 function createShader(gl, type, source) {
-  var shader = gl.createShader(type);
+  let shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (success) {
     return shader;
   }
@@ -61,11 +62,11 @@ function createShader(gl, type, source) {
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
-  var program = gl.createProgram();
+  let program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
-  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (success) {
     return program;
   }
@@ -75,3 +76,26 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
   return null;
 }
+
+let vertString =
+`// an attribute will receive data from a buffer
+ attribute vec4 a_position;
+
+ // all shaders have a main function
+ void main() {
+
+   // gl_Position is a special variable a vertex shader
+   // is responsible for setting
+   gl_Position = a_position;
+ }`;
+
+ let fragString =
+ `// fragment shaders don't have a default precision so we need
+  // to pick one. mediump is a good default
+  precision mediump float;
+
+  void main() {
+    // gl_FragColor is a special variable a fragment shader
+    // is responsible for setting
+    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+  }`
