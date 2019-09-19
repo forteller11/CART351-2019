a4e30af8273b6a3d668a3089e0843d0d06676539
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2019-09-19 08:39:20 -0400

    updated personal website to add exercise02

--- js/createGraph.js
+++ js/createGraph.js
@@ -11,7 +11,11 @@ function createGraph(){
         let eb = new Node("Website", e1, "https://forteller11.github.io/CART351-2019/");
         let ec = new Node("lastparent", e1);
         e1.children.push(ea, eb)
-      let e2 = new Node("Exercise02_(placeholder)", exercises);
+      let e2 = new Node("Exercise02", exercises);
+        let e2Github = new Node ("Github", e2, "https://github.com/forteller11/CART351-2019/tree/master/exercises/exercise02");
+        let e2A = new Node ("PartA", e2, "https://forteller11.github.io/CART351-2019/exercises/exercise02/PartA/index.html");
+        let e2B = new Node ("PartB", e2, "https://forteller11.github.io/CART351-2019/exercises/exercise02/PartB/index.html");
+        e2.children.push(e2A, e2B, e2Github);
       let e3 = new Node("Exercise03_(placeholder)", exercises);
       exercises.children.push(e1, e2, e3);
     let projects = new Node("Project", root);
