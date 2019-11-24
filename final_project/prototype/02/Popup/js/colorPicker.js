6ce36c980e9c78cea124702e9a913a0a9fc1a86a
forteller11 <charly.yan.miller@gmail.com>
2019-11-24 01:17:25 -0500

    starting popup

--- final_project/prototype/02/Popup/js/colorPicker.js
+++ final_project/prototype/02/Popup/js/colorPicker.js
@@ -2,8 +2,12 @@
 window.onload = main;
 
 function main(){
-  let brush = document.getElementById("brush");
-
+  let canvas = document.getElementById("colorPicker");
+  let ctx = canvas.getContext("2d");
+  console.log(canvas);
+  console.log(ctx);
+  ctx.fillStyle = rgbaCol(255,0,0,1);
+  ctx.fillRect(0,0, canvas.width, canvas.height);
   brush.addEventListener('click', ()=>{
     console.log("brush clicked");
     //change tool to brush on current tab (or all tabs?)
@@ -13,7 +17,11 @@ function main(){
 
   clear.addEventListener('click', ()=>{
     console.log("canvas cleared");
-    
+
     //change tool to brush on current tab (or all tabs?)
   });
+}
+
+function rgbaCol(r,g,b,a){
+  return "rgba("+r+","+g+","+b+","+a+")";
 }
